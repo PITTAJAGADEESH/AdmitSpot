8cfcbf02eafcce1520cdf51dd0cdcef179d28f70
PITTAJAGADEESH <suryajagadeesh192@gmail.com>
2024-10-25 13:05:08 +0530

    Initial commit

--- db/database.js
+++ db/database.js
@@ -1,32 +1,28 @@
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(process.env.DATABASE_URL);
+const Database = require("better-sqlite3");
+const db = new Database(process.env.DATABASE_URL, { verbose: console.log });
 
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      email TEXT UNIQUE NOT NULL,
-      password TEXT NOT NULL,
-      email_verified INTEGER DEFAULT 0
-    )
-  `);
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    email_verified INTEGER DEFAULT 0
+  );
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS contacts (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      email TEXT UNIQUE NOT NULL,
-      phone TEXT NOT NULL,
-      address TEXT,
-      timezone TEXT,
-      user_id INTEGER,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      updated_at DATETIME,
-      deleted_at DATETIME,
-      FOREIGN KEY (user_id) REFERENCES users(id)
-    )
-  `);
-});
+  CREATE TABLE IF NOT EXISTS contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phone TEXT NOT NULL,
+    address TEXT,
+    timezone TEXT,
+    user_id INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  );
+`);
 
 module.exports = db;
